6b1c5463136aaca55ecb4f36f0265746ace25e1b
paolo.carrasco <paolo.carrasco@gmail.com>
2017-09-19 22:10:37 -0500

    se pintan ciudades configuradas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,12 @@
 const mostrarCiudades = ciudades => {
-  ciudades.forEach(ciudad => console.log(ciudad.nombre));
+  const listadoCiudades = [];
+  ciudades.forEach(ciudad => {
+    const htmlCiudad = `<h4>${ciudad.nombre}</h4>
+          <p data-id="2147714"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
+          <p>${ciudad.descripcion}</p>`;
+    listadoCiudades.push(htmlCiudad);
+  });
+  const contenedorCiudades = document.querySelector('.row.marketing');
+  contenedorCiudades.innerHTML = listadoCiudades.join('');
 };
-
 export default mostrarCiudades;
