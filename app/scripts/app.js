f3e70102b1a9ccde60d40448ae8045dd99092c6e
GlisseLisbeth <glisselisbeth@gmail.com>
2017-09-21 17:19:59 -0500

    Paint cities and error 401 API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,20 @@
+
 const mostrarCiudades = ciudades => {
+  const url = 'http://api.openweathermap.org/data/2.5/forecast/city';
+  const keys = 'eb412ece183748e0700ba1711bf25710';
+
   const listadoCiudades = [];
   ciudades.forEach(ciudad => {
+    fetch(`${url}?id=${ciudad.id}&APPID=${keys};units=metric`, {
+      headers: { Accept: 'application/json'}
+    }).then(resultado => {
+        resultado.json().then(contenido => {
+            // destructuring de objetos
+            console.log("mostrar contenido"+contenido);
+        });
+    });
     const htmlCiudad = `<h4>${ciudad.nombre}</h4>
-          <p data-id="2147714"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
+          <p data-id="${ciudad.id}"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
           <p>${ciudad.descripcion}</p>`;
     listadoCiudades.push(htmlCiudad);
   });
