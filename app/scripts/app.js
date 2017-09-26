705114d5336ada9e8f750c3cea6f6faa4c78d53a
paolo.carrasco <paolo.carrasco@gmail.com>
2017-09-26 01:13:47 -0500

    city template is surrounded by div

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,12 @@
 const mostrarCiudades = ciudades => {
   const listadoCiudades = [];
   ciudades.forEach(ciudad => {
-    const htmlCiudad = `<h4>${ciudad.nombre}</h4>
+    const htmlCiudad = `
+        <div class="col-md-6">
+          <h4>${ciudad.nombre}</h4>
           <p data-id="2147714"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
-          <p>${ciudad.descripcion}</p>`;
+          <p>${ciudad.descripcion}</p>
+        </div>`;
     listadoCiudades.push(htmlCiudad);
   });
   const contenedorCiudades = document.querySelector('.row.marketing');
